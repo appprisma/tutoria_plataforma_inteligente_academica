f530adcf0eb964f04ce71e75b557b331fb2958a4
appprisma <app.prisma@ibime.edu.mx>
2026-04-14 08:11:44 -0600

    Rename FIREBASECONFIG to firebaseconfig
    Renamed FIREBASECONFIG to firebaseconfig for consistency.

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@
 //  → General → Tus apps → SDK de la app y configuración
 // ============================================================
 
-const FIREBASECONFIG = {
+const firebaseconfig = {
   apiKey: "AIzaSyCkSgBxKaZBEDrF_RhFGCK6tZjlzdTQt10",
   authDomain: "er-reporte---3er-periodo-44119.firebaseapp.com",
   databaseURL: "https://er-reporte---3er-periodo-44119-default-rtdb.firebaseio.com",
